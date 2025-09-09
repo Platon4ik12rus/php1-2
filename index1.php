8775cdba84aa01c61a42c2e6f69636f96e6b7510
Студент П4 <user_p4@mrmt.loc>
2025-09-09 18:27:36 +0300

    zad

---
+++ index1.php
@@ -0,0 +1,10 @@
+<?php
+$arr1 = readline("Введите 1 число: ");
+$arr2 = readline("Введите 2 число: ");
+$arr3 = readline("Введите 3 число: ");
+
+if ( $arr2 - $arr1 === $arr3 - $arr2){
+echo "yes";}
+else{
+echo "no";
+}
